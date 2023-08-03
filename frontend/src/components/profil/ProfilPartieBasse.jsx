7a6c017bc34dc82afa62bea9b9e64cd5b42b5ad3
Djok64 <joakym.anc@gmail.com>
2023-08-03 16:26:49 +0200

    partie basse gauche finalisé debut du css

--- frontend/src/components/profil/ProfilPartieBasse.jsx
+++ frontend/src/components/profil/ProfilPartieBasse.jsx
@@ -10,23 +10,23 @@ function ProfilPartieBasse() {
   return (
     <>
       <div className="GlobalContainerPartieBasseGauche">
-        <div className="BlocNumero1">
+        <div className="BlocNumero">
           <img src={icone1} alt="icone person details" />{" "}
           <div className="PersonnalDetails"> Personal details</div>
         </div>
-        <div className="BlocNumero2">
+        <div className="BlocNumero">
           <img src={icone2} alt="icone Payment History" />{" "}
           <div className="PaymentHistory"> Payment History </div>
         </div>
-        <div className="BlocNumero3">
+        <div className="BlocNumero">
           <img src={icone3} alt="icone Inbox" />{" "}
           <div className="Inbox"> Inbox </div>
         </div>
-        <div className="BlocNumero4">
+        <div className="BlocNumero">
           <img src={icone4} alt="icone Key" />{" "}
           <div className="ResetPassword"> Reset Password </div>
         </div>
-        <div className="BlocNumero5">
+        <div className="BlocNumero">
           <img src={icone5} alt="icone Logout" />{" "}
           <div className="Logout">Logout </div>
         </div>
