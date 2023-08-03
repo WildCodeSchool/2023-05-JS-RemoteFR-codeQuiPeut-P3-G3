6efa5a52f509ba42c2ddee1ad726a52b380da491
Ben-git01 <benbakirbenjamin@orange.fr>
2023-08-03 15:05:21 +0200

    create navbar and responsive css

--- frontend/src/components/global/Navbar.jsx
+++ frontend/src/components/global/Navbar.jsx
@@ -1,24 +1,59 @@
-import "./NavBar.scss"
+import "./Navbar.scss"
 import { Link } from "react-router-dom"
+import logo from "../../assets/images/logoviolet.png"
+import account from "../../assets/images/account.png"
+import profileicon from "../../assets/images/profileicon.png"
+import { useState } from "react"
 
 function NavBar() {
+  const [isExploreOpen, setIsExploreOpen] = useState(false)
+
+  const handleDropdown = () => {
+    setIsExploreOpen(!isExploreOpen)
+  }
   return (
     <>
-      <p> Navbar temporaire </p>
-      <ul>
-        <li>
-          <Link to="/"> Accueil </Link>
-        </li>
-        <li>
-          <Link to="/admin"> Admin </Link>
-        </li>
-        <li>
-          <Link to="/profile"> Profil </Link>
-        </li>
-        <li>
-          <Link to="/games"> Jeux </Link>
-        </li>
-      </ul>
+      <div className="navbar-container">
+        <Link to="/">
+          <input type="image" src={logo} alt="logo" className="logo-navbar" />
+        </Link>
+        {/* <div className="explore-nav">
+          <select name="Explore" id="navbar-select">
+            <option value="explore">Explore</option>
+          </select>
+        </div> */}
+        <div className="dropdown-navbar">
+          <button className="dropbtn-navbar" onClick={handleDropdown}>
+            Explore
+            <i className="fa fa-caret-down"></i>
+          </button>
+          {isExploreOpen && (
+            <div className="dropdown-content-navbar" id="myDropdown">
+              <a href="#">Link 1</a>
+              <a href="#">Link 2</a>
+              <a href="#">Link 3</a>
+            </div>
+          )}
+        </div>
+        <input type="text" placeholder="search" className="search-navbar" />
+        <div className="profile-navbar">
+          <img src={account} alt="logo" className="left-img-navbar" />
+          <img src={profileicon} alt="logo" className="right-img-navbar" />
+        </div>
+      </div>
+      <div className="bottom-menu" id="bottomMenu">
+        <ul>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/profile">
+            <li>Profile</li>
+          </Link>
+          <Link to="/games">
+            <li>Games</li>
+          </Link>
+        </ul>
+      </div>
     </>
   )
 }
