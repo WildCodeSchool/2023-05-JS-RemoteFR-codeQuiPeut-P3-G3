d67df94cb44db40f3e633347e891ebbe3f7266f2
ANTOINE_NAME TUJAS_NAME <antoine.tujas33@gmail.com>
2023-08-04 11:04:50 +0200

    problem resolved

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import "./Home.scss"
+import StarRating from "../components/home/CarouselNote"
 
 export default function Home() {
   return (
@@ -7,12 +8,11 @@ export default function Home() {
       <main className="home__content">
         <section className="home__content__description"></section>
         <section className="home__content__games"></section>
-        <section className="home__content__avis"></section>
+        <section className="home__content__avis">
+          <StarRating />
+        </section>
       </main>
-
-      {/* FOOTER */}
       <footer>
-        {/* -> Footer à ajouter ici */}
         <h3 style={{ textAlign: "center" }}> footer </h3>
       </footer>
     </>
