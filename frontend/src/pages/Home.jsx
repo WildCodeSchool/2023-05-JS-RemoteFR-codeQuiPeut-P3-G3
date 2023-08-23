720c19a74287cd5f6040ac5054611eb2c451346c
catherineohrel <catherine_ohrel@hotmail.com>
2023-08-23 10:48:32 +0200

    parallax pas fini

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import HomeParallax from "../components/home/HomeParallax"
+// import HomeParallax from "../components/home/HomeParallax"
 import "./Home.scss"
 import Footer from "../components/global/Footer"
+import noParallaxImg from "../assets/images/parallax/castleNewSize1.png"
 export default function Home() {
   return (
     <>
       <section className="home__hero">
-        <HomeParallax />
+        {/* <HomeParallax /> */}
+        <img className="noParallax" src={noParallaxImg} alt="castle" />
       </section>
       <main className="home__content">
         <section className="home__content__description"></section>
