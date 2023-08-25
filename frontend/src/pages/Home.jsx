7be24abdc4833ca8c878611d219e29bffff91789
catherineohrel <catherine_ohrel@hotmail.com>
2023-08-25 11:45:30 +0200

    cascade image dans home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,12 +5,17 @@ import StarRating from "../components/home/CarouselNote"
 
 import Footer from "../components/global/Footer"
 import noParallaxImg from "../assets/images/parallax/castleNewSize1.png"
+import cascadeImg from "../assets/images/cascadeImg.png"
+
 export default function Home() {
   return (
     <>
       <section className="home__hero">
         {/* <HomeParallax /> */}
-        <img className="noParallax" src={noParallaxImg} alt="castle" />
+        <div className="image-container">
+          <img className="noParallaxImg" src={noParallaxImg} alt="castle" />
+          <img className="cascadeImg" src={cascadeImg} alt="cascade" />
+        </div>
       </section>
       <main className="home__content">
         <section className="home__content__description">
