2a711365394ad89a0d85ba3a032509648ad6202c
Ben-git01 <benbakirbenjamin@orange.fr>
2023-08-31 11:24:47 +0200

    signup and login but still some work to do

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,22 @@
 import "./Login.scss"
 import userpic from "../assets/images/user2.png"
+import { useState } from "react"
 function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    // TODO: Call a login function here
+  }
+
   return (
     <div className="container-login">
       <div className="top-login">
@@ -9,18 +25,25 @@ function Login() {
         <div className="logo-montagne-login"></div>
       </div>
       <div className="bot-login">
-        <input type="email" placeholder="email" className="input-login" />
+        <input
+          type="email"
+          placeholder="email"
+          className="input-login"
+          onChange={handleEmailChange}
+        />
         <input
           type="password"
           placeholder="password"
           className="input-login"
           id="password-login"
+          onChange={handlePasswordChange}
         />
         <input
           type="button"
           placeholder="submit"
           className="button-login"
           value="S'identifier"
+          onClick={handleSubmit}
         />
       </div>
     </div>
