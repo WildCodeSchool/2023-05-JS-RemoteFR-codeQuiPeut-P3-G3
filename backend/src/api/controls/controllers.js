7b486dad8b5837655a2f9ead10755b67ea1d1c4c
Antoine Elissagaray <antoine.elissagaray@orange.fr>
2023-09-19 11:01:42 +0200

    render des scenes ok + background ok

--- backend/src/api/controls/controllers.js
+++ backend/src/api/controls/controllers.js
@@ -93,6 +93,7 @@ module.exports.createStory = (req, res) => {
 
 module.exports.putScene = (req, res) => {
   const { idStory, idScene } = req.params
+  console.error("enregistrement de ", idStory, " scene ", idScene)
   const sceneContent = req.body
 
   const filePath = findPath(idStory)
