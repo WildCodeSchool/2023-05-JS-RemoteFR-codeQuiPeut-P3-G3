6cb152e062714a19dba0ce83e7e730d41aca8a32
Ben-git01 <benbakirbenjamin@orange.fr>
2023-09-22 10:19:34 +0200

    Some minor fixes before meeting

---
+++ backend/src/api/stories/168-toto.js
@@ -0,0 +1,202 @@
+module.exports.story = {
+  "global": {
+    "description": "",
+    "name": "",
+    "category": "",
+    "public": ""
+  },
+  "scenes": [
+    {
+      "textbox": {
+        "9be4aa87-65e9-41cf-8c3f-0694fd7f40f7": {
+          "obj": {
+            "type": "textbox",
+            "version": "5.3.0",
+            "originX": "left",
+            "originY": "top",
+            "left": 394,
+            "top": 181.9,
+            "width": 200,
+            "height": 45.2,
+            "fill": "black",
+            "stroke": null,
+            "strokeWidth": 1,
+            "strokeDashArray": null,
+            "strokeLineCap": "butt",
+            "strokeDashOffset": 0,
+            "strokeLineJoin": "miter",
+            "strokeUniform": false,
+            "strokeMiterLimit": 4,
+            "scaleX": 1,
+            "scaleY": 1,
+            "angle": 0,
+            "flipX": false,
+            "flipY": false,
+            "opacity": 1,
+            "shadow": null,
+            "visible": true,
+            "backgroundColor": "",
+            "fillRule": "nonzero",
+            "paintFirst": "fill",
+            "globalCompositeOperation": "source-over",
+            "skewX": 0,
+            "skewY": 0,
+            "fontFamily": "Times New Roman",
+            "fontWeight": "normal",
+            "fontSize": 40,
+            "text": "Yo",
+            "underline": false,
+            "overline": false,
+            "linethrough": false,
+            "textAlign": "left",
+            "fontStyle": "normal",
+            "lineHeight": 1.16,
+            "textBackgroundColor": "",
+            "charSpacing": 0,
+            "styles": [],
+            "direction": "ltr",
+            "path": null,
+            "pathStartOffset": 0,
+            "pathSide": "left",
+            "pathAlign": "baseline",
+            "minWidth": 20,
+            "splitByGrapheme": false
+          },
+          "Actions": [
+            {
+              "type": "add",
+              "target": "life",
+              "number": "1"
+            }
+          ],
+          "id": "9be4aa87-65e9-41cf-8c3f-0694fd7f40f7",
+          "link": "1",
+          "pos": {
+            "percX": 31.444533120510776,
+            "percY": 25.808282344595195
+          }
+        },
+        "e7963c1f-70d5-4144-8de6-37111245381d": {
+          "obj": {
+            "type": "textbox",
+            "version": "5.3.0",
+            "originX": "left",
+            "originY": "top",
+            "left": 829,
+            "top": 181.79,
+            "width": 200,
+            "height": 97.63,
+            "fill": "black",
+            "stroke": null,
+            "strokeWidth": 1,
+            "strokeDashArray": null,
+            "strokeLineCap": "butt",
+            "strokeDashOffset": 0,
+            "strokeLineJoin": "miter",
+            "strokeUniform": false,
+            "strokeMiterLimit": 4,
+            "scaleX": 1,
+            "scaleY": 1,
+            "angle": 0,
+            "flipX": false,
+            "flipY": false,
+            "opacity": 1,
+            "shadow": null,
+            "visible": true,
+            "backgroundColor": "",
+            "fillRule": "nonzero",
+            "paintFirst": "fill",
+            "globalCompositeOperation": "source-over",
+            "skewX": 0,
+            "skewY": 0,
+            "fontFamily": "Times New Roman",
+            "fontWeight": "normal",
+            "fontSize": 40,
+            "text": "Loose moneyy",
+            "underline": false,
+            "overline": false,
+            "linethrough": false,
+            "textAlign": "left",
+            "fontStyle": "normal",
+            "lineHeight": 1.16,
+            "textBackgroundColor": "",
+            "charSpacing": 0,
+            "styles": [],
+            "direction": "ltr",
+            "path": null,
+            "pathStartOffset": 0,
+            "pathSide": "left",
+            "pathAlign": "baseline",
+            "minWidth": 20,
+            "splitByGrapheme": false
+          },
+          "Actions": [
+            {
+              "type": "subs",
+              "target": "money",
+              "number": "3"
+            }
+          ],
+          "id": "e7963c1f-70d5-4144-8de6-37111245381d",
+          "link": "",
+          "pos": {
+            "percX": 66.16121308858739,
+            "percY": 25.79255077562538
+          }
+        }
+      },
+      "background": "http://localhost:4242/uploads/Calque2_1695367468682.png"
+    },
+    {
+      "background": "http://localhost:4242/uploads/heart_1695367486847.png"
+    }
+  ],
+  "heroes": [
+    {
+      "class": "b",
+      "img": null,
+      "name": "Mic",
+      "heal": 4,
+      "maxHeal": 4,
+      "money": 100,
+      "unit": "dollar",
+      "equipped": {
+        "weapons": []
+      },
+      "inventory": {
+        "weapons": [],
+        "items": []
+      },
+      "skills": {
+        "agility": "15",
+        "strength": "15",
+        "intelligence": "15",
+        "resistance": "15"
+      }
+    },
+    {
+      "class": "b",
+      "img": null,
+      "name": "Mich",
+      "heal": 4,
+      "maxHeal": 4,
+      "money": 100,
+      "unit": "dollar",
+      "equipped": {
+        "weapons": []
+      },
+      "inventory": {
+        "weapons": [],
+        "items": []
+      },
+      "skills": {
+        "agility": "15",
+        "strength": "15",
+        "intelligence": "15",
+        "resistance": "15"
+      }
+    }
+  ],
+  "weapons": [],
+  "consumables": []
+};
