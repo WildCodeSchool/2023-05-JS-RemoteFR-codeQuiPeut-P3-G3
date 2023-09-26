d9e6baf71a654c8f87f509e7dc033ef61b74d4da
NayanN91 <nayan.nanouche@gmail.com>
2023-09-26 19:58:59 +0200

    el commito de la muerte

--- backend/src/api/stories/167-LE_DECLIN_DES_AMES.js
+++ backend/src/api/stories/167-LE_DECLIN_DES_AMES.js
@@ -1,18 +1,63 @@
 module.exports.story = {
-  "global": {
-    "description": "",
-    "name": "",
-    "category": "",
-    "public": ""
+  global: {
+    description: "",
+    name: "",
+    category: "",
+    public: "",
   },
-  "scenes": [
+  scenes: [
     {
-      "textbox": {},
-      "image": {},
-      "rect": {}
-    }
+      textbox: {},
+      image: {},
+      rect: {},
+    },
   ],
-  "heroes": [],
-  "weapons": [],
-  "consumables": []
-};
+  heroes: [
+    {
+      class: "jacques",
+      img: null,
+      name: "jean",
+      heal: 4,
+      maxHeal: 4,
+      money: 100,
+      unit: "",
+      equipped: {
+        weapons: [],
+      },
+      inventory: {
+        weapons: [],
+        items: [],
+      },
+      skills: {
+        agility: "20",
+        strength: 0,
+        intelligence: 0,
+        resistance: 0,
+      },
+    },
+    {
+      class: "benslimane",
+      img: "610-AsQRa4L_1695373748700.png",
+      name: "bilel",
+      heal: "6",
+      maxHeal: 4,
+      money: "500",
+      unit: "",
+      equipped: {
+        weapons: [],
+      },
+      inventory: {
+        weapons: [],
+        items: [],
+      },
+      skills: {
+        agility: "10",
+        strength: "100",
+        intelligence: "100",
+        resistance: "25",
+      },
+    },
+  ],
+  weapons: [],
+  consumables: [],
+}
